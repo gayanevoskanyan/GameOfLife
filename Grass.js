7634622949fb09bf5cfd9654bf0da60ba8a2b424
gayanevoskanyan <gayane.voskanyan.y@tumo.org>
2022-03-13 19:38:57 +0400

    second

---
+++ Grass.js
@@ -0,0 +1,15 @@
+class Grass extends LivingCreature {          //indexy 1
+    mul() {
+      this.multiply++;
+      let emptyCell = this.chooseCell(0);
+      let newCell = emptyCell[Math.floor(Math.random() * emptyCell.length)];
+      if (newCell && this.multiply >= 8) {
+        let newX = newCell[0];
+        let newY = newCell[1];
+        matrix[newX][newY] = 1;
+        let gr = new Grass(newX, newY);
+        grassArr.push(gr);
+        this.multiply = 0;
+      }
+    }
+  }
